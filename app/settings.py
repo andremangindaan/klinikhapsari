6289ccb0803b1bdd396db32475c2bb33fb8797ca
andremangindaan <andres.mangindaan@gmail.com>
2021-10-14 19:02:01 +0700

    Changing debug

--- app/settings.py
+++ app/settings.py
@@ -28,14 +28,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ze-ab7i9ls^bk!$md@(8%is-zet5tls!93&4x4gt21xvxppkh@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-if len(sys.argv) >= 2 and sys.argv[1] == 'runserver':
-    DEBUG = True
-else:
-    DEBUG = False
+# if len(sys.argv) >= 2 and sys.argv[1] == 'runserver':
+#     DEBUG = True
+# else:
+#     DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
